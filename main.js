e60b528b224ba6fd62d4fae259cee24775c8b3da
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2020-08-06 11:08:57 -0400

    message

---
+++ main.js
@@ -0,0 +1,16 @@
+let message= document.getElementById('message');
+let btn=document.getElementById("btn");
+
+let message1=`Mainu ni pata what happened...  but i dont think bad of you and na kadi galat sochea kuch.....  i respect all your decisions and ma bas eh clear krna c ki  j tenu lagda ki kite ma
+Kuch kehta galat kyuki kai cheej hundi bnda kehnda kisi hor tareeke nal but dooja bnda kuch hor samaj lenda..  and all..  eda haini kuch.... 
+i didnt messaged u direct u know why maybe😂..bas ehna hi... and haan maybe i gave negative vibes taahii  hun eda di cheeja v dekhi ma hundi jo maybe nahi honi ....chal  anyways ...i just wanted to make it clear in your head.... chal  best of luck for you ahead.. Be positive everytime..:) ...
+`
+let count=0;
+btn.addEventListener('click',messa);
+
+function messa(){
+
+if(count==0)
+    message.append(message1);
+    count++;
+}
